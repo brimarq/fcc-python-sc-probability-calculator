3ddb2d60cca4b6223cab0da25ec5d296ecf98ff0
Brian Marquis <rbmarquis.dev@gmail.com>
2021-09-10 00:28:04 -0500

    define experiment func

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,23 +1,43 @@
 import copy
 import random
-# Consider using the modules imported above.
 
 class Hat:
     def __init__(self, **kwargs):
-        # Nested list comprehensions are NOT intuitive! 
-        # for/if clauses nest in the same order as if written 
-        # with for loops and if statements. See PEP 202: 
-        # https://www.python.org/dev/peps/pep-0202/#the-proposed-solution
         self.contents = [
-          key for key in kwargs for _ in range(kwargs[key]) 
+          color for color in kwargs for _ in range(kwargs[color]) 
         ]
         
-        
+    def draw(self, num_balls):
+        drawn_balls = []
 
+        if num_balls > len(self.contents): 
+            return self.contents
+
+        for _ in range(num_balls):
+            drawn_balls.append(
+                self.contents.pop(
+                    self.contents.index(
+                        random.choice(self.contents)
+                    )
+                )
+            )
+ 
+        return drawn_balls
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
 
-hat = Hat(black=6, red=4, green=3)
-print(hat.contents)
+    success = 0
+
+    for _ in range(num_experiments):
+        hat_copy = copy.deepcopy(hat)
+        drawn_balls = hat_copy.draw(num_balls_drawn)
+
+        matching_drawn_balls = {
+            k: drawn_balls.count(k) for k, v in expected_balls.items() if drawn_balls.count(k) >= v
+        }
+
+        if len(matching_drawn_balls) == len(expected_balls):
+            success += 1
+
+    return success / num_experiments
